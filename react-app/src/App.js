290ae10b2e57d9eaa426b03a954e6b47fba61c52
Muhammed Fahad <muhammedfahadh@outlook.com>
2023-11-09 08:39:15 +0530

    Route Map Setup Finished

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,9 +1,37 @@
 import NavBar from "./Components/Navbar/NavBar";
+import {BrowserRouter, Routes, Route} from "react-router-dom"
+import Shop from "./Pages/Shop"
+import ShopCategory from "./Pages/ShopCategory"
+import Product from "./Pages/Product"
+import Cart from "./Pages/Cart"
+import LoginSignup from "./Pages/LoginSignup"
+
 
 function App() {
   return (
     <div>
+      <BrowserRouter>
+
       <NavBar/>
+
+      <Routes>
+
+        <Route path="/" element={<Shop/>}/>
+        <Route path="/HeadDrops" element={<ShopCategory category="HeadDrops"/>}/>
+        <Route path="/OnEar" element={<ShopCategory category="OnEar"/>}/>
+        <Route path="/BarSound" element={<ShopCategory category="BarSound"/>}/>
+
+        <Route path="/Product" element= {<Product/>}>
+          <Route path=":productId"  element={<Product/>}/>
+        </Route>
+
+        <Route path="/Cart" element={<Cart/>}/>
+
+        <Route path="/Login" element={<LoginSignup/>}/>
+
+      </Routes>
+
+      </BrowserRouter>
     </div>
   );
 }
--- react-app/src/Components/Navbar/NavBar.jsx
+++ react-app/src/Components/Navbar/NavBar.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import './NavBar.css'
 import logo from "../Assets/logo.png"
 import cart_icon from "../Assets/cart_icon.png"
+import user_pic from "../Assets/user_pic.png"
 
 const NavBar = () => {
 
@@ -15,13 +16,16 @@ const NavBar = () => {
         <p>EarDrum</p>
       </div>
       <ul className="nav-menu">
-        <li onClick={()=>{setMenu("HeadDrops")}}>HeadDrops {menu==="HeadDrops" ? <hr/> : <></> }</li>
-        <li onClick={()=>{setMenu("OverHead")}}>OverHead {menu==="OverHead" ? <hr/> : <></> }</li>
-        <li onClick={()=>{setMenu("OnEar")}}>OnEar {menu==="OnEar" ? <hr/> : <></> }</li>
-        <li onClick={()=>{setMenu("BarSound")}}>BarSound {menu==="BarSound" ? <hr/> : <></> }</li>
+        <li onMouseEnter={()=>{setMenu("Shop")}}>Shop {menu==="Shop" ? <hr/> : <></> }</li>
+        <li onMouseEnter={()=>{setMenu("HeadDrop")}}>HeadDrop {menu==="HeadDrop" ? <hr/> : <></> }</li>
+        <li onMouseEnter={()=>{setMenu("OnEar")}}>OnEar {menu==="OnEar" ? <hr/> : <></> }</li>
+        <li onMouseEnter={()=>{setMenu("BarSound")}}>BarSound {menu==="BarSound" ? <hr/> : <></> }</li>
       </ul>
       <div className="nav-login-cart">
         <button>Login</button>
+        <div className="user-profile">
+        <img src={user_pic} alt="" className='user_profile_pic'/>
+        </div>
         <img src={cart_icon} alt="cart_icon" />
         <div className="nav-cart-counter">0</div>
       </div>
